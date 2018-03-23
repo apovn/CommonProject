d825001cec5d4b34172560cfe118bc2e74c84693
apovn <tritranduc@gmail.com>
2018-03-23 16:25:22 +0700

    interface contain method implement

--- src/main/java/designpattern/factory/EncryptionAlgorithm.java
+++ src/main/java/designpattern/factory/EncryptionAlgorithm.java
@@ -2,4 +2,8 @@ package designpattern.factory;
 
 public interface EncryptionAlgorithm {
     String encrypt(String plaintext);
+
+    static String test(String plaintext) {
+        return "";
+    }
 }
